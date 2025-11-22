9eb1e04d880ab4f5df04c127df56f7098311c9ae
gunbamguy <gbamguy@gmail.com>
2025-11-22 22:39:55 +0900

    Update player list with new player '윤가놈.gif'

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // js/config.js
 
 const PLAYERS = {
-    top: ['김뿡.gif', '룩삼.gif', '얍얍.gif', '운타라.gif', '한동숙.gif'],
+    top: ['김뿡.gif', '룩삼.gif', '윤가놈.gif', '운타라.gif', '한동숙.gif'],
     mid: ['랄로.gif', '앰비션.gif', '인섹.gif', '트롤야.gif', '피닉스박.gif'],
     bot: ['괴물쥐.gif', '따효니.gif', '러너.gif', '명예훈장.gif', '삼식.gif'],
     sup: ['던.gif', '소풍왔니.gif', '순당무.gif', '인간젤리.gif', '푸린.gif']
@@ -18,3 +18,4 @@ const POSITION_NAMES = {
 function getPlayerName(filename) {
     return filename.replace('.gif', '');
 }
+
